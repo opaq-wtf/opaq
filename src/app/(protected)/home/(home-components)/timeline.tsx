029c60ebee336a123ff05f390f89cb8d3edbc081
Mahadev234 <tmsankaram@gmail.com>
2025-06-28 23:51:46 +0530

    feat: add Loader component for improved loading state display in ArtwallBody

--- src/app/(protected)/home/(home-components)/timeline.tsx
+++ src/app/(protected)/home/(home-components)/timeline.tsx
@@ -7,6 +7,7 @@ import { Heart, Share2, Bookmark, Flag, Tag, MessageCircle } from 'lucide-react'
 import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
 import axios from 'axios';
+import Loader from '@/components/common/Loader';
 
 interface Post {
   _id: string;
@@ -284,12 +285,7 @@ export function ArtwallBody() {
 
   if (loading && posts.length === 0) {
     return (
-      <div className="flex items-center justify-center min-h-[400px]">
-        <div className="text-center">
-          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mx-auto mb-4"></div>
-          <p className="text-gray-400">Loading posts...</p>
-        </div>
-      </div>
+      <Loader />
     );
   }
 
